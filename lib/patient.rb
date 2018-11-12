f328acdcad73fabc36d6191ecf5a22ba2f48fe65
Sean Langton <slangton1@gmail.com>
2018-11-12 20:40:00 +0000

    automatically backed up by learn

---
+++ lib/patient.rb
@@ -0,0 +1,27 @@
+class Genre
+
+  @@all = []
+  attr_accessor :name
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_appointment(doctor, date)
+    Appointment.new(self, doctor, date)
+  end
+
+  def appointments
+    Appointment.all.select {|patient| appointment.patient == self}
+  end
+
+  def doctors
+    self.appointments.collect {|doctor| appointment.doctor}
+  end
+
+end
