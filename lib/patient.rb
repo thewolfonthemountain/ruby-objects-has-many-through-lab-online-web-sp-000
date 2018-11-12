fe995ae1ba9152bbf7d56f64f1ecbbecbd6d9517
Sean Langton <slangton1@gmail.com>
2018-11-12 20:51:09 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -17,7 +17,7 @@ class Patient
   end
 
   def appointments
-    Appointment.all.select {|patient| appointment.patient == self}
+    Appointment.all.select {|appointment| appointment.patient == self}
   end
 
   def doctors
